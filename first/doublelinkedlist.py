76d1ddf7cf71b28d0a4535a0beb52abb3c493089
ermolenko-alexander <ermolenko.alexander@gmail.com>
2023-03-18 18:58:21 +0300

    Инициализация двусвязного списка. Функция поиска первого элемента по значению.

---
+++ first/doublelinkedlist.py
@@ -0,0 +1,47 @@
+class Node:
+    def __init__(self, v):
+        self.value = v
+        self.prev = None
+        self.next = None
+
+
+class LinkedList2:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_in_tail(self, item):
+        if self.head is None:
+            self.head = item
+            item.prev = None
+            item.next = None
+        else:
+            self.tail.next = item
+            item.prev = self.tail
+        self.tail = item
+
+    def find(self, val):
+        curr = self.head
+        while curr is not None:
+            if curr.value == val:
+                return curr
+            curr = curr.next
+        return None
+
+    def find_all(self, val):
+        return []
+
+    def delete(self, val, all=False):
+        pass
+
+    def clean(self):
+        pass
+
+    def len(self):
+        return 0
+
+    def insert(self, afterNode, newNode):
+        pass
+
+    def add_in_head(self, newNode):
+        pass
