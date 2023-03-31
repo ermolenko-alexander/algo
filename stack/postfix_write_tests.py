14404198b7352390852fafca37147c994edc4be9
ermolenko-alexander <ermolenko.alexander@gmail.com>
2023-03-31 19:37:48 +0300

    Тесты на постфиксную запись.

---
+++ stack/postfix_write_tests.py
@@ -0,0 +1,17 @@
+import pytest
+from postfix_write import *
+
+
+@pytest.mark.parametrize('input_exp, expected_result', [
+    ('8 2 + 4 - =', 6), ('8 2 + 5 * 9 + =', 59), ('2 3 - 3 *', -3), ('1 1 * 2 +', 3)
+])
+def test_get_result_ok(input_exp, expected_result):
+    assert expression(input_exp) == expected_result
+
+
+@pytest.mark.parametrize('input_exp', [
+    '1 2 + v =', '! 69 32 1 * -'
+])
+def test_get_result_exception(input_exp):
+    with pytest.raises(ValueError):
+        assert expression(input_exp)
